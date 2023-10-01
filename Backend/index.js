8d29a33af4200f6165f579b75dc474eece18e9b4
sanath.rs <sanath.rs@zohocorp.com>
2023-10-01 21:00:29 +0530

    Retrieve API

--- Backend/index.js
+++ Backend/index.js
@@ -6,30 +6,56 @@ const app = express();
 app.use(express.json());
 
 let LONG_URL_VS_SHORT_URL = new Map();
-let SHORT_URL_GENERATED = new Set();
+let SHORT_URL_VS_LONG_URL = new Map();
 
-function shorten(req, res) {
+function shortenURL(req, res) {
+    let statusCode = null;
+    let responseJson = null;
+    
     let longURL = req.body.long_url;
+    let shortenedURL;
     if (LONG_URL_VS_SHORT_URL.has(longURL)) {
-        res.send({
-            'shortened_url': LONG_URL_VS_SHORT_URL.get(longURL)
-        });
+        shortenedURL = LONG_URL_VS_SHORT_URL.get(longURL);
     } else {
-        let shortenedURL = randomstring.generate(5);
-        while (SHORT_URL_GENERATED.has(shortenedURL))
-        {
+        shortenedURL = randomstring.generate(5);
+        while (SHORT_URL_VS_LONG_URL.has(shortenedURL)) {
             shortenedURL = randomstring.generate(5);
         }
         LONG_URL_VS_SHORT_URL.set(longURL, shortenedURL);
-        SHORT_URL_GENERATED.add(shortenedURL, longURL);
-        res.send({
-            'shortened_url': shortenedURL
-        });
-        // console.log("res is", res);
+        SHORT_URL_VS_LONG_URL.set(shortenedURL, longURL);
     }
+    statusCode = 200;
+    responseJson = {
+        'short_url': shortenedURL
+    }
+    res.status(statusCode).send(responseJson);
 }
 
-app.post('/shorten', shorten);
+
+
+function retrieveLongURL(req, res) {
+    let statusCode = null;
+    let responseJson = null;
+
+    let shortURL = req.body.short_url;
+    let longURL = SHORT_URL_VS_LONG_URL.get(shortURL);
+    if (longURL == null) {
+        statusCode = 400;
+        responseJson = {
+            'Error': 'long url does not exist!'
+        };
+    } else {
+        statusCode = 200;
+        responseJson = {
+            'long_url': longURL
+        }
+    }
+
+    res.status(statusCode).send(responseJson);
+}
+
+app.post('/shorten', shortenURL);
+app.get('/retrieve', retrieveLongURL);
 
 const port = 3005;
 app.listen(port);
